1029b05949367fc2596c07f664387289e87ad8fc
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-16 12:18:33 +0300

    auth imported

--- src/_steps.js
+++ src/_steps.js
@@ -1,10 +1,21 @@
 /*
+------------------------------------------------------------------------
+Basic context API setUP
+-------------------------------------------------------------------------
+
 1- Context-API: share state with other components(accross the components);
 2- Create an user Context
 3- ContextProvider with passed children
 4- set the UserContext in the index.js
 5- To consume the context: export the userContext from UserContext.js file
 6- to access the userContext we need use useContext hook.
-7- So at the header or any where else: use useContext hook.
+7- So at the header or any where else: use useContext hook to access the value from userContext.
+
+------------------------------------------------------------------------
+Firebase auth intregation
+-------------------------------------------------------------------------
+1- use getAuth by passing the app from firebase configuration app
+2- create a function called createUser and return createUserWithEmailAndPassword
+3- 
 
 */
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,9 +1,13 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import app from '../firebase/firebase.config';
+import { getAuth } from 'firebase/auth'
 
 export const AuthContext = createContext();
 
+const Auth = getAuth(app)
+
 const UserContext = ({children}) => {
-    const user ={displayName: 'Tauhid'};
+    const [user, setUser] = useState({displayName: 'Tauhid'});
     const authInfo = {user: user};
     return (
         <AuthContext.Provider value={authInfo}>
